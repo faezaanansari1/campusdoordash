e879f88c5cea279befb1d8a03a11ce18c455dee9
Youssef Tafesh <ytafesh1@umbc.edu>
2025-10-29 15:14:27 -0400

    backend: added express server, mongoose db connection, cors/cookie-parser setup, and updated package.json

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,23 @@
-const express = require("express");
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import connectDB from './config/db.js';  
+import 'dotenv/config';             
+
+
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+await connectDB()
+
+const allowedOrigins = ['http://localhost:5173']
+
+app.use(express.json());
+app.use(cors({origin: allowedOrigins, credentials: true}));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('hello world');
 });
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on port 5000`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
