bd8506ac27f6bb4fb6e778e5525f3cfe46cb81db
faezaanansari1 <fansari1@umbc.edu>
2025-11-02 19:40:08 -0500

    Filtering and searching for catalog, styling

--- frontend/src/pages/Catalog.jsx
+++ frontend/src/pages/Catalog.jsx
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
 import './Catalog.css'
 import Card from '../components/Card'
 import chickimg from "../assets/chickfila1.png"
@@ -35,18 +36,84 @@ const Catalog = () => {
         "img": dunkinimg,
         "loc": "Commons",
         "desc": "Your spot for coffee/espresso drinks, donuts, bagels, and breakfast sandwiches."
+      },
+      {
+        "id": 5,
+        "name": "Einstein Bros. Bagel",
+        "img": dunkinimg,
+        "loc": "AOK Library",
+        "desc": "Bagels, sandwiches, coffee, and breakfast staples located in the AOK Library."
       }
-
     ];
 
+  const [mainVendors, setMainVendors] = useState([]);
+  const [searchedVendors, setSearchedVendors] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedLocation, setSelectedLocation] = useState("");
+
+  const handleSelect = (location) => {
+    setSelectedLocation(location);
+    console.log("Selected:", location);
+  };
+
+  // Gets all vendors (catalog data) from db
+  // TODO: Read vendors from db
+  const getVendors = () => {
+    setMainVendors(catalogData);
+  };
+
+  // When page initially loads, get all vendors (catalog data)
+  useEffect(() => {
+      getVendors();
+  }, [])
+
+  // TODO: Move search functionality to parent as search can occur on multiple pages
+  // Recalculate searchedPosts when searchTerm changes, which occurs when user types something in search box
+  useEffect(() => {
+    console.log("Recalculating searchedPosts");
+    // If no search term, searchedVendors=mainVendors.
+    if (!searchTerm) {
+      setSearchedVendors(mainVendors);
+    // If search term, then searchedVendors is set to all vendors matching the search
+    } else {
+      const results = mainVendors.filter((item) =>
+        item.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setSearchedVendors(results);
+    }
+  }, [searchTerm, mainVendors]);
 
   return (
     <div className="catalog">
       <h1>Our Catalog</h1>
+
+      <div className='filters'>
+        <div className="search-box">
+          <input
+            type="text"
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Search for a restaurant"
+          />
+        </div>
+
+        <div className="dropdown">
+          <button className="dropbtn">Location</button>
+          <div className="dropdown-content">
+            <Link to="/catalog" onClick={() => handleSelect("")}>All</Link>
+            <Link to="/catalog" onClick={() => handleSelect("Commons")}>Commons</Link>
+            <Link to="/catalog" onClick={() => handleSelect("University Center")}>University Center</Link>
+            <Link to="/catalog" onClick={() => handleSelect("AOK Library")}>AOK Library</Link>
+          </div>
+        </div>
+      </div>
+
       <div className="container">
-        {/* <div> */}
-          {        
-          catalogData.map((member, index) => (
+        {        
+          searchedVendors
+            .filter(member =>
+              selectedLocation === "" || member.loc === selectedLocation
+            )
+            .map((member, index) => (
             <Card key={index}
                 id={member.id}
                 name={member.name}
@@ -55,10 +122,9 @@ const Catalog = () => {
                 desc={member.desc}
             />
           ))
-          }      
-        {/* </div> */}
+        }      
       </div>
-    </div>
+  </div>
   );
 };
 
